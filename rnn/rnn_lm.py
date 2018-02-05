6d88357fd0d83d249a1b41b76524f9d0574ad7fc
yangfengguang <yangfengguang@baidu.com>
2018-02-05 15:22:16 +0800

    add rnn_lm

--- rnn/rnn_lm.py
+++ rnn/rnn_lm.py
@@ -19,13 +19,20 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
 import os
 import csv
 import itertools
 
 import nltk
 import numpy as np
+
+home_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+bin_dir = os.path.join(home_dir, 'bin')
+model_dir = os.path.join(home_dir, 'model')
+conf_dir = os.path.join(home_dir, 'conf')
+data_dir = os.path.join(home_dir, 'data')
+lib_dir = os.path.join(home_dir, 'lib')
+log_dir = os.path.join(home_dir, 'log')
 
 vocabulary_size = 8000
 unknown_token = "UNKNOWN_TOKEN"
@@ -34,7 +41,7 @@ sentence_end_token = "SENTENCE_END"
 
 # Read the data and append SENTENCE_START and SENTENCE_END tokens
 print "Reading CSV file..."
-with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+with open('../data/reddit-comments-2015-08.csv', 'rb') as f:
     reader = csv.reader(f, skipinitialspace=True)
     reader.next()
     # Split full comments into sentences
@@ -70,3 +77,34 @@ print "\nExample sentence after Pre-processing: '%s'" % tokenized_sentences[0]
 # Create the training data
 X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
 y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+
+
+class RNNNumpy:
+    def __init__(self, word_dim, hidden_dim=100, bptt_truncate=4):
+        # Assign instance variables
+        self.word_dim = word_dim
+        self.hidden_dim = hidden_dim
+        self.bptt_truncate = bptt_truncate
+        # Randomly initialize the network parameters
+        self.U = np.random.uniform(-np.sqrt(1. / word_dim), np.sqrt(1. / word_dim), (hidden_dim, word_dim))
+        self.V = np.random.uniform(-np.sqrt(1. / hidden_dim), np.sqrt(1. / hidden_dim), (word_dim, hidden_dim))
+        self.W = np.random.uniform(-np.sqrt(1. / hidden_dim), np.sqrt(1. / hidden_dim), (hidden_dim, hidden_dim))
+
+    def forward_propagation(self, x):
+        # The total number of time steps
+        T = len(x)
+        # During forward propagation we save all hidden states in s because need them later.
+        # We add one additional element for the initial hidden, which we set to 0
+        s = np.zeros((T + 1, self.hidden_dim))
+        s[-1] = np.zeros(self.hidden_dim)
+        # The outputs at each time step. Again, we save them for later.
+        o = np.zeros((T, self.word_dim))
+        # For each time step...
+        for t in np.arange(T):
+            # Note that we are indxing U by x[t]. This is the same as multiplying U with a one-hot vector.
+            s[t] = np.tanh(self.U[:, x[t]] + self.W.dot(s[t - 1]))
+            o[t] = softmax(self.V.dot(s[t]))
+        return [o, s]
+
+
+RNNNumpy.forward_propagation = forward_propagation
